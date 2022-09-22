1da5f0188b2e4aa977d04ee80273c804d4f5c427
Saladin Afoh <safoh@student.codam.nl>
2022-09-22 17:23:59 +0200

    fixed first zombie

--- ex00/include/Zombie.hpp
+++ ex00/include/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: safoh <safoh@student.codam.nl>             //   \ \ __| | | \ \/ /   */
 /*                                                 (|     | )|_| |_| |>  <    */
 /*   Created: 2022/09/21 18:41:49 by safoh        /'\_   _/`\__|\__,_/_/\_\   */
-/*   Updated: 2022/09/21 19:04:10 by safoh        \___)=(___/                 */
+/*   Updated: 2022/09/22 17:23:42 by safoh        \___)=(___/                 */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@ class	Zombie {
 
 	public:
 	Zombie(std::string name);
+	void	announce(void) const;
 	~Zombie();
 
 	private:
-	void	announce(void) const;
 	Zombie();
 	std::string	_name;
 };
